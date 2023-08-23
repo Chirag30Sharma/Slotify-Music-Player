06e6b8b1ab15d9a4b8dc39803498cc5c805d1921
Chirag Sharma <92788732+Chirag30Sharma@users.noreply.github.com>
2023-08-23 18:58:38 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,12 +1,1 @@
-<?php
-	ob_start();
-	session_start();
 
-	$timezone = date_default_timezone_set("Indian/Maldives");
-
-	$con = mysqli_connect("sql12.freesqldatabase.com", "sql12593294", "ytEMRP8PnK", "sql12593294");
-
-	if(mysqli_connect_errno()) {
-		echo "Failed to connect: " . mysqli_connect_errno();
-	}
-?>
